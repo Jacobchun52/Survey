d55fe3920faf2471e69954087648c35962ba4daf
Jacobchun52 <jacobchun520@gmail.com>
2024-05-15 21:13:02 +0800

    url.js

--- url.js
+++ url.js
@@ -8,5 +8,8 @@
 
 
 const urls = [
-		'https://www.surveycake.com/s/KaQ4N'
+	'https://www.surveycake.com/s/NGw3D',
+	'https://www.surveycake.com/s/lY6o3',
+	'https://www.surveycake.com/s/64RWW',
+	'https://www.surveycake.com/s/BzVQg'
 ];
