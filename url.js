6975408facae3bd06b88a5c471638f066891de77
Jacobchun52 <jacobchun520@gmail.com>
2024-05-18 01:00:26 +0800

    url.js

--- url.js
+++ url.js
@@ -1,8 +1,11 @@
 
 const urls = [
-	'https://www.surveycake.com/s/NGw3D',
-	'https://www.surveycake.com/s/BzVQg',
-	'https://www.surveycake.com/s/BzVQg',
-	'https://www.surveycake.com/s/BzVQg',
-	'https://www.surveycake.com/s/BzVQg'
+	'https://www.surveycake.com/s/NGRYy',
+	'https://www.surveycake.com/s/obk02',
+	'https://www.surveycake.com/s/PaqOM',
+	'https://www.surveycake.com/s/y2vge',
+	'https://www.surveycake.com/s/NGRYy',
+	'https://www.surveycake.com/s/obk02',
+	'https://www.surveycake.com/s/PaqOM',
+	'https://www.surveycake.com/s/y2vge'
 ];
