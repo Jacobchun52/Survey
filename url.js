b66f1e0bda82b9b0771ad59c165dc25295ea1926
Jacobchun52 <jacobchun520@gmail.com>
2024-05-23 14:11:58 +0800

    url.js

--- url.js
+++ url.js
@@ -4,7 +4,8 @@ const urls = [
 	'https://www.surveycake.com/s/zm3e2', 
 	'https://www.surveycake.com/s/BzxWw', 
 	'https://www.surveycake.com/s/zm3e2', 
-	'https://www.surveycake.com/s/MazlV', 
+	'https://www.surveycake.com/s/MazlV',
+	'https://www.surveycake.com/s/zm3e2', 
 	'https://www.surveycake.com/s/BzxWw',
 	'https://www.surveycake.com/s/BzxWw'
 ];
