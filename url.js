b8d0eb30ba2f07ce530b34e028c538346002e918
Jacobchun52 <jacobchun520@gmail.com>
2024-05-30 16:45:19 +0800

    url.js

--- url.js
+++ url.js
@@ -1,10 +1,21 @@
 
 const urls = [
-	'https://www.surveycake.com/s/m912n', 
-	'https://www.surveycake.com/s/XDMpV', 
-	'https://www.surveycake.com/s/64QY2', 
-	'https://www.surveycake.com/s/qLD2d', 
-	'https://www.surveycake.com/s/64QY2', 
-	'https://www.surveycake.com/s/64QY2', 
-	'https://www.surveycake.com/s/64QY2'
+'https://www.surveycake.com/s/AVevD',
+'https://www.surveycake.com/s/PaQ4w,
+'https://www.surveycake.com/s/WRM1v,
+'https://www.surveycake.com/s/XDQQ8,
+
+
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7',
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7',
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7',
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7',
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7',
+'https://www.surveycake.com/s/9yYYx',
+'https://www.surveycake.com/s/17we7'
 ];
