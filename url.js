60b219738d161c7e2010fab988c6b73aa1cfde1e
Jacobchun52 <jacobchun520@gmail.com>
2024-07-05 13:50:50 +0800

    url.js

--- url.js
+++ url.js
@@ -20,17 +20,18 @@ const urls = [
 'https://www.surveycake.com/s/VaaQR',
  
 
-/* 串流 */
+/* 串流 'https://www.surveycake.com/s/obbvV',*/
 'https://www.surveycake.com/s/YaaL7',
 'https://www.surveycake.com/s/KaavN',
 'https://www.surveycake.com/s/NGGrB',
-'https://www.surveycake.com/s/obbvV',
 'https://www.surveycake.com/s/Rgg81',
 'https://www.surveycake.com/s/DaaOX',
 'https://www.surveycake.com/s/vo7XO',
+'https://www.surveycake.com/s/vo7XO',
+'https://www.surveycake.com/s/vo7XO',
 'https://www.surveycake.com/s/YaaL7',
 'https://www.surveycake.com/s/NGGrB',
-'https://www.surveycake.com/s/obbvV',
+'https://www.surveycake.com/s/Rgg81',
 'https://www.surveycake.com/s/vo7XO'
 
 ];
