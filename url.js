fc4afed43e4192f12e347133cd2437f8859709f6
Jacobchun52 <jacobchun520@gmail.com>
2024-07-06 13:07:40 +0800

    url.js

--- url.js
+++ url.js
@@ -12,20 +12,19 @@ const urls = [
 'https://www.surveycake.com/s/7LLxN',
 'https://www.surveycake.com/s/e0gdz',
   
-/* 電商平台 */
+/* 電商平台 
 'https://www.surveycake.com/s/gKg1L',
 'https://www.surveycake.com/s/Rgg4y',
-
-/* 電信公司 */
-'https://www.surveycake.com/s/qLgBa',
+*/
+  
+/* 電信公司 'https://www.surveycake.com/s/qLgBa',*/
 'https://www.surveycake.com/s/VaaQR',
 'https://www.surveycake.com/s/VaaQR', 
 'https://www.surveycake.com/s/VaaQR', 
   
-/* 串流 */
+/* 串流 'https://www.surveycake.com/s/NGGrB', */
 'https://www.surveycake.com/s/YaaL7',
 'https://www.surveycake.com/s/KaavN',
-'https://www.surveycake.com/s/NGGrB',
 'https://www.surveycake.com/s/Rgg81',
 'https://www.surveycake.com/s/DaaOX',
 'https://www.surveycake.com/s/vo7XO',
